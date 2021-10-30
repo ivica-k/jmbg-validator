1f872e99294faba898bbe8c36208d885f8dddb8f
ivica-k <ivica.kolenkas@gmail.com>
2021-10-30 11:09:14 +0200

    fix: configures GH action to release to PyPI

--- setup.py
+++ setup.py
@@ -10,7 +10,8 @@ setup(
     version=__version__,
     description="Validira i ispisuje osnovne podatke o JMBGu. EN: Validates and displays basic UMCN data.",
     author="Ivica Kolenkaš",
-    url="https://github.com/ivica-k",
+    author_email="ivica.kolenkas@gmail.com",
+    url="https://github.com/ivica-k/jmbg-validator",
     python_requires=">=3.7",
     packages=["jmbg_validator"],
     license="MPL2.0",
@@ -19,4 +20,14 @@ setup(
     entry_points={
         "console_scripts": ["jmbg-validator=jmbg_validator.main:cli"],
     },
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
 )
